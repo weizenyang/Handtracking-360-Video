ba18375a7a8f79ca2fa52bd89d7f4afac64dc660
weizenyang <weizenyang@gmail.com>
2023-10-27 17:42:15 +0100

    ad

--- src/layouts/scripts/event-manager-360.js
+++ src/layouts/scripts/event-manager-360.js
@@ -29,13 +29,17 @@ AFRAME.registerComponent('event-manager', {
       console.log(this.videoSphere.components.material.data.src)
       if(this.videoSphere.components.material.data.src == `#${targetEl.id}`){
         console.log("clear")
-        this.videoSphere.components.material.data.src = ""
-        console.log(this.videoSphere.components.material.data.src)
-        console.log("target " + `#${targetEl.id}`)
+        // this.videoSphere.components.material.data.src = ""
+        document.querySelector(`sphere-${targetEl.id}`).object3D.scale = "0 0 0"
+        document.querySelector(`sphere-${targetEl.id}`).components.material.material.map.image.stop();
       } else {
         console.log("change id")
-        this.videoSphere.components.material.data.src = `#${targetEl.id}`
-        this.videoSphere.components.material.material.map.image.play();
+        document.querySelectorAll("a-videosphere").forEach((e)=> {
+          e.object3D.scale = "0 0 0"
+          e.components.material.material.map.image.stop();
+        })
+        document.querySelector(`sphere-${targetEl.id}`).object3D.scale = "1 1 1"
+        document.querySelector(`sphere-${targetEl.id}`).components.material.material.map.image.play();
         console.log(this.videoSphere.components.material.data.src)
         console.log("target " + `#${targetEl.id}`)
       }
