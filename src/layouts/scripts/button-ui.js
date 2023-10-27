6db22ca171227b502010b260869be872b663593d
weizenyang <weizenyang@gmail.com>
2023-10-27 22:24:05 +0100

    Afds

---
+++ src/layouts/scripts/button-ui.js
@@ -0,0 +1,116 @@
+/* global AFRAME */
+AFRAME.registerComponent('button', {
+    schema: {
+      label: {default: 'label'},
+      width: {default: 0.11},
+      toggable: {default: false}
+    },
+    init: function () {
+      var el = this.el;
+      var labelEl = this.labelEl = document.createElement('a-entity');
+      this.triggerdown = false
+      this.intersected = false
+      this.color = '#3a50c5';
+      // el.setAttribute('geometry', {
+      //   primitive: 'box',
+      //   width: this.data.width,
+      //   height: 0.05,
+      //   depth: 0.04
+      // });
+  
+      // el.setAttribute('material', {color: this.color});
+      el.setAttribute('pressable', '');
+  
+      labelEl.setAttribute('position', '0 0 0.02');
+      labelEl.setAttribute('text', {
+        value: this.data.label,
+        color: 'white',
+        align: 'center'
+      });
+  
+      labelEl.setAttribute('scale', '0.75 0.75 0.75');
+      this.el.appendChild(labelEl);
+  
+      this.bindMethods();
+      
+      this.controllers = document.querySelectorAll(".controller").forEach((e) => {
+        e.addEventListener("triggerdown", (e) => {
+          this.triggerdown = true
+          if(this.intersected){
+            this.onPressedStarted
+          }
+        })
+
+        e.addEventListener("triggerup", (e) => {
+          this.triggerdown = false
+          if(this.intersected){
+            this.onPressedEnded
+          }
+        })
+      })
+      
+      this.el.addEventListener('stateadded', this.stateChanged);
+      this.el.addEventListener('stateremoved', this.stateChanged);
+      this.el.addEventListener('pressedstarted', this.onPressedStarted);
+      this.el.addEventListener('pressedhovered', this.onPressedHovered);
+      this.el.addEventListener('pressedended', this.onPressedEnded);
+      this.el.addEventListener('raycaster-intersected', (e)=>{
+        this.intersected = true
+        this.stateChanged
+      });
+      this.el.addEventListener('raycaster-intersected-cleared', (e) => {
+        this.intersected = false
+        this.stateChanged
+        this.onPressedEnded
+      });
+    },
+  
+    bindMethods: function () {
+      this.stateChanged = this.stateChanged.bind(this);
+      this.onPressedStarted = this.onPressedStarted.bind(this);
+      this.onPressedEnded = this.onPressedEnded.bind(this);
+    },
+  
+    update: function (oldData) {
+      if (oldData.label !== this.data.label) {
+        this.labelEl.setAttribute('text', 'value', this.data.label);
+      }
+    },
+  
+    stateChanged: function () {
+      // var color = this.el.is('pressed') ? 'green' : this.color;
+      // this.el.setAttribute('material', {color: color});
+    },
+  
+    onPressedStarted: function () {
+      var el = this.el;
+      // el.setAttribute('material', {color: 'green'});
+      el.emit('click');
+      if (this.data.togabble) {
+        if (el.is('pressed')) {
+          el.removeState('pressed');
+        } else {
+          el.addState('pressed');
+        }
+      }
+    },
+
+    onHoverStarted: function () {
+      var el = this.el;
+      // el.setAttribute('material', {color: 'green'});
+      el.emit('hover');
+      el.querySelector(".border").object3D.visible = false;
+    },
+
+    onHoverEnded: function () {
+      var el = this.el;
+      // el.setAttribute('material', {color: 'green'});
+      el.emit('hover');
+        el.querySelector(".border").object3D.visible = false;
+    },
+  
+    onPressedEnded: function () {
+      if (this.el.is('pressed')) { return; }
+      this.el.setAttribute('material', {color: this.color});
+    }
+  });
---
+++ src/layouts/scripts/pressable-ui.js
@@ -0,0 +1,50 @@
+/* global AFRAME, THREE */
+AFRAME.registerComponent('pressable', {
+    schema: {
+      pressDistance: { default: 0.06 },
+      hoverDistance: { default: 0.11}
+    },
+  
+    init: function () {
+      this.worldPosition = new THREE.Vector3();
+      this.handEls = document.querySelectorAll('[hand-tracking-controls]');
+      this.pressed = false;
+      this.hovered = false;
+    },
+  
+    tick: function () {
+      var handEls = this.handEls;
+      var handEl;
+      var distance;
+      for (var i = 0; i < handEls.length; i++) {
+        handEl = handEls[i];
+        distance = this.calculateFingerDistance(handEl.components['hand-tracking-controls'].indexTipPosition);
+        if (distance < this.data.pressDistance) {
+          if (!this.pressed) { this.el.emit('pressedstarted'); }
+          this.pressed = true;
+          this.hovered = false;
+          return;
+        } else if(distance < this.data.hoverDistance){
+          if (!this.hovered) { this.el.emit('pressedhovered', {"distance": distance}); }
+          this.hovered = true;
+        } else{
+          this.hovered = false;
+        }
+      }
+      if (this.pressed) { this.el.emit('pressedended'); }
+      if (this.hovered) { this.el.emit('hoverended'); }
+      this.pressed = false;
+      
+    },
+  
+    calculateFingerDistance: function (fingerPosition) {
+      var el = this.el;
+      var worldPosition = this.worldPosition;
+  
+      worldPosition.copy(el.object3D.position);
+      el.object3D.parent.updateMatrixWorld();
+      el.object3D.parent.localToWorld(worldPosition);
+  
+      return worldPosition.distanceTo(fingerPosition);
+    }
+  });
